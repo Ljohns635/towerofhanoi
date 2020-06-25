d96ad0efd6f2a709e61741afb145d52223610b77
LJohnson <latishaj966@gmail.com>
2020-06-24 17:08:15 -0700

    got change const for the variables so the code runs correctly

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
-//Recieved layout from Randy
-let start = 'pick items up' //pick up
-let destination = null
+const rod1 = document.querySelector('#rod1')
+const rod2 = document.querySelector('#rod2')
+const rod3 = document.querySelector('#rod3')
+
+let disc1 = document.querySelector('#disc1')
+let disc2 = document.querySelector('#disc2')
+let disc3 = document.querySelector('#disc3')
+let disc4 = document.querySelector('#disc4')
+
+let destinationStart = "pick item up"
+let destinationEnd = null
+
 const moveIt = function (evt) {
-//displays what is happening when clicked
-// console.log('target ' + evt.target.id)
-// console.log('currentTarget ' + evt.currentTarget.id)
-const rod = evt.currentTarget
-//gives a conditional if no stone
-if (start === 'pick items up') {
-  //pick up
-  const stone= rod.lastElementChild
-  if (stone) {
-    console.log('you pick up stone ' + stone.id)
-    destination = stone
-    start = 'drop'
-  }else {
-    console.log('no disc to pick up')
-  }
-}else{
-  //drop
-  console.log('You dropped onto ' + rod.id)
-  rod.append(destination)
-  mode = 'pick items up'
+  let choices;
+  console.log('target: ' + evt.target.id)
+  console.log('currentTarget: ' + evt.currentTarget.id)
+  const rod = evt.currentTarget
+  const stone = rod.lastElementChild
+  if (destinationStart === 'pick item up') {
+    //pick
+    if (stone) {
+      console.log('You picked up a stone: ' + stone.id)
+      destinationEnd = stone
+      destinationStart = 'dropped it'
+    } else {
+      console.log('No disc to pick up')
+    }
+  } else {
+      console.log('You dropped item onto ' + rod.id)
+      rod.append(destinationEnd)
+      destinationStart = 'pick item up'
+    } 
 }
-}
-
-let rod1 = document.querySelector('#rod1')
-let rod2 = document.querySelector('#rod2')
-let rod3 = document.querySelector('#rod3')
-
 
 rod1.addEventListener('click', moveIt)
 rod2.addEventListener('click', moveIt)
